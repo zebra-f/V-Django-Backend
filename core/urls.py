e315f61d4764d86a80e84cc347b60576ac2d1466
rafal <zebraf.dev@gmail.com>
2023-03-07 00:25:00 +0100

    removed simple-jwt urls

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenBlacklistView
-)
 
 from .auth.views import CustomTokenRefreshView, LogoutView, LoginView
 from .users.urls import router as users_router
@@ -21,10 +16,7 @@ urlpatterns = [
     
     path('api/', include(router.urls)),
 
-    path('api/token/obtainpair/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
-    path('api/token/obtainpair/login/', LoginView.as_view(), name='login'),
-    path('api/token/blacklist/logout/', LogoutView.as_view(), name='logout'),
-    path('api/token/blacklist/refresh/', CustomTokenRefreshView.as_view(), name='refresh')
+    path('api/token/login/', LoginView.as_view(), name='login'),
+    path('api/token/logout/', LogoutView.as_view(), name='logout'),
+    path('api/token/refresh/', CustomTokenRefreshView.as_view(), name='refresh')
 ]
