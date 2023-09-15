132a6ec4d38d211eb44f080132dc27fcde4b847a
rafal <zebraf.dev@gmail.com>
2023-09-16 00:55:53 +0200

    feat: add filterting by tags, add tags validator

--- core/speeds/filters.py
+++ core/speeds/filters.py
@@ -1,9 +1,47 @@
-from django_filters import rest_framework as filters
+from django.utils.datastructures import MultiValueDict
+from django_filters import rest_framework as filters, widgets
 
 from .models import Speed
+from .validators import tags_validator
+
+
+class CustomQueryArrayWidget(widgets.QueryArrayWidget):
+
+    def value_from_datadict(self, data, files, name):
+        # source code: leave it as is just in case; 
+        # it appears that it won't ever run as the `data` should be always a QueryDict
+        if not isinstance(data, MultiValueDict):
+            data = data.copy()
+            for key, value in data.items():
+                # treat value as csv string: ?foo=1,2
+                if isinstance(value, str):
+                    data[key] = [x.strip() for x in value.rstrip(",").split(",") if x]
+            data = MultiValueDict(data)
+
+        values_list = data.getlist(name, data.getlist("%s[]" % name)) or []
+
+        # add the following `if` statement
+        if len(values_list) == 1:
+            if isinstance(values_list[0], str):
+                ret = values_list[0].lower().split(',')
+            else:
+                ret = []
+        # replace the `if len(values_list) > 0` with the following line
+        elif len(values_list) > 1:
+            # add `.lower()` method to the following line
+            # add `and isinstance(x, str)` to the following line
+            ret = [x.lower() for x in values_list if x and isinstance(x, str)]  
+        else:
+            ret = []
+        return list(set(ret))
 
 
 class SpeedFilter(filters.FilterSet):
+    tags = filters.Filter(
+        widget=CustomQueryArrayWidget,
+        method='tags_filter',
+        label='Speed tags, comma separated (example: `tag1,tag2,tag3`):'
+    )
 
     class Meta:
         model = Speed
@@ -11,4 +49,9 @@ class SpeedFilter(filters.FilterSet):
             'is_public', 
             'speed_type', 
             'user__username',
+            'tags'
             ]
+        
+    def tags_filter(self, queryset, name: str, value: list):
+        tags_validator(value, max_length=4)
+        return queryset.filter(tags__contains=value)
--- core/speeds/validators.py
+++ core/speeds/validators.py
@@ -1,4 +1,8 @@
+import re
+
 from django.core.validators import MinValueValidator, RegexValidator
+from django.utils.deconstruct import deconstructible
+from rest_framework import serializers
 
 
 # Models
@@ -9,7 +13,31 @@ class CustomMinValueValidator(MinValueValidator):
         return a <= b 
     
 
-# Serializers
+# Serializers/Filters
+
+@deconstructible
+class TagsValidator:
+
+    def __call__(self, tags: list, max_length: int=4):
+        if not isinstance(tags, list):
+            raise serializers.ValidationError("The data type provided is incorrect; it should be a list.")
+        if len(tags) > max_length:
+            raise serializers.ValidationError(f"Maximum limit exceeded. The list can have a maximum of {max_length} items.")
+
+        for tag in tags:
+            if not isinstance(tag, str):
+                raise serializers.ValidationError("The data's item type provided is incorrect; it should be a string.")
+            if len(tag) > 20:
+                raise serializers.ValidationError("Tag length exceeds the maximum limit. Each tag inside can have a maximum of 20 characters.")
+            
+            pattern = r"^[a-zA-Z0-9'-]+$"
+            if not re.fullmatch(pattern, tag):
+                raise serializers.ValidationError(
+                    "Tags must consist of letters (both uppercase and lowercase), numbers, and the following symbols (excluding the next dot): ' - ."
+                    )
+
+# Speed.tags (ArrayField)
+tags_validator = TagsValidator()
 
 # Speed.name (CharField)
 name_validator = RegexValidator(
@@ -46,3 +74,4 @@ detail_validator = RegexValidator(
         "the category should be between 1 and 256 characters in length." 
     )
 )
+
