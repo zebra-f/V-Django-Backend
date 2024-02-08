c1cb3ff35be358bade1ba9ffeb69ffa2098c3d65
rafal <zebraf.dev@gmail.com>
2024-02-08 12:58:52 +0100

    fix: replace dict with a list for tags validation error message

--- core/speeds/validators.py
+++ core/speeds/validators.py
@@ -17,38 +17,28 @@ class CustomMinValueValidator(MinValueValidator):
 
 class TagsValidator:
     def __call__(self, tags: list[str], max_length: int = 4):
-        error_messages = {}
+        error_messages = []
         valid = True
 
         if not isinstance(tags, list):
-            error_messages[
-                "incorrect_data_type"
-            ] = "The data type provided is incorrect; it should be a list."
+            error_messages.append("The data type provided is incorrect; it should be a list.")
             valid = False
         if isinstance(tags, list) and len(tags) > max_length:
-            error_messages[
-                "incorect_data_item_type"
-            ] = f"Maximum limit exceeded. The list can have a maximum of {max_length} items."
+            error_messages.append(f"Maximum limit exceeded. The list can have a maximum of {max_length} items.")
             valid = False
 
         if isinstance(tags, list):
             for tag in tags:
                 if not isinstance(tag, str):
-                    error_messages[
-                        "incorect_data_item_type"
-                    ] = "The data's item type provided is incorrect; it should be a string."
+                    error_messages.append("The data's item type provided is incorrect; it should be a string.")
                     valid = False
                 if isinstance(tag, str) and len(tag) > 20:
-                    error_messages[
-                        "item_too_long"
-                    ] = "Tag length exceeds the maximum limit. Each tag can have a maximum of 20 characters."
+                    error_messages.append("Tag length exceeds the maximum limit. Each tag can have a maximum of 20 characters.")
                     valid = False
 
                 pattern = r"^[a-zA-Z0-9'-]+$"
                 if isinstance(tag, str) and not re.fullmatch(pattern, tag):
-                    error_messages[
-                        "string_contains_invalid_character"
-                    ] = "Tags must consist of letters (both uppercase and lowercase), numbers, and the following symbols (excluding the next dot): ' - ."
+                    error_messages.append("Tags must consist of letters (both uppercase and lowercase), numbers, and the following symbols (excluding the next dot): ' - .")
                     valid = False
 
         if not valid:
