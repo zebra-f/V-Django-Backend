15bd47381a46b28c9ad08b31b70cc83767e94ba2
rafal <zebraf.dev@gmail.com>
2024-03-05 22:22:42 +0100

    chore: set up static files, add gunicorn for prod

--- core/settings.py
+++ core/settings.py
@@ -143,7 +143,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "django_static"
+STATIC_URL = "/django_static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
