a5d3a913c2c65c7dde397bcc8950cd56fba51435
rafal <zebraf.dev@gmail.com>
2024-03-06 20:52:05 +0100

    feat: add new fields with links to speed admin

--- core/speeds/admin.py
+++ core/speeds/admin.py
@@ -2,6 +2,8 @@ from typing import Any
 from django.contrib import admin
 from django.db.models.query import QuerySet
 from django.http.request import HttpRequest
+from django.utils.html import format_html
+from django.urls import reverse
 
 from .models import Speed, SpeedFeedback, SpeedBookmark, SpeedReport
 from .queries import SpeedQueries
@@ -21,6 +23,7 @@ class SpeedAdmin(admin.ModelAdmin):
     list_display = [
         "id",
         "user",
+        "user_id",
         "downvotes",
         "upvotes",
         "score",
@@ -34,6 +37,12 @@ class SpeedAdmin(admin.ModelAdmin):
         "created_at",
         ReportsCountFilter,
     ]
+
+    def user_id(self, obj):
+        url = reverse("admin:users_user_change", args=[obj.user.id])
+        return format_html(
+            "<a href='{url}'>{user}</a>", url=url, user=str(obj.user.id)[:10] + "..."
+        )
 
     def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
         return SpeedQueries.get_admin_site_query()
@@ -90,7 +99,18 @@ class SpeedReportAdmin(admin.ModelAdmin):
         "id",
         "user",
         "speed",
+        "speed_id",
+        "report_reason",
+        "reported_speed_user",
     ]
+
+    def speed_id(self, obj):
+        url = reverse("admin:speeds_speed_change", args=[obj.speed.id])
+        return format_html("<a href='{url}'>{user}</a>", url=url, user=obj.speed.id)
+
+    def reported_speed_user(self, obj):
+        url = reverse("admin:users_user_change", args=[obj.speed.user.id])
+        return format_html("<a href='{url}'>{user}</a>", url=url, user=obj.speed.user)
 
     def has_add_permission(self, request, obj=None):
         return False
